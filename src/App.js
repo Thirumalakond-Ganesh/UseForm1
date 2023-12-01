7faf8a220ac539dd41a3149bcf376f1cd11648b2
Thirumalakonda-Ganesh <ganitthirumala50@gmail.com>
2023-12-01 11:50:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import './App.css';
 
-function App() {
+
+function MyForm() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const onSubmit = (data) => {
+    console.log('Form data submitted:', data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        Name:
+        <input {...register('name', { required: true })} />
+      </label>
+
+      <label>
+        Email:
+        <input {...register('email', { required: true, pattern: /^\S+@\S+$/i })} />
+      </label>
+
+      <button type="submit">Submit</button>
+
+      {/* Display form state information */}
+      <div>
+        <p>Is Dirty: {formState.isDirty ? 'Yes' : 'No'}</p>
+        <p>Is Submitted: {formState.isSubmitted ? 'Yes' : 'No'}</p>
+        <p>Submit Count: {formState.submitCount}</p>
+        <p>Is Submitting: {formState.isSubmitting ? 'Yes' : 'No'}</p>
+        <p>Is Valid: {formState.isValid ? 'Yes' : 'No'}</p>
+        <p>Is Validating: {formState.isValidating ? 'Yes' : 'No'}</p>
+      </div>
+    </form>
   );
 }
 
-export default App;
+export default MyForm;
